ac5f09294838520340f7cdd88eeee5eb7dc7c1bb
usama bin ajmal <ajmalusama25@gmail.com>
2022-01-11 15:31:43 +0500

    added variable

--- cheesHub/cheesHub/ViewController.swift
+++ cheesHub/cheesHub/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var usam = "123"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
